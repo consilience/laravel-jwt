9ef928a5b71a42739221602db0f981b2706349de
Michael Stokoe <mstokoe0990@gmail.com>
2019-04-16 10:18:43 +0100

    Package Skeleton Ready

---
+++ src/LaravelJwtServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Consilience\LaravelJWT;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class LaravelJwtServiceProvider extends BaseServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/jwt.php' => config_path('jwt.php'),
+        ]);
+    }
+
+    public function register()
+    {
+        //
+    }
+}

ade21019430fbda236c43de4b620cc3e24a38099
Michael Stokoe <mstokoe0990@gmail.com>
2019-04-16 15:30:43 +0100

    Added create/get Token

--- src/JWTService.php
+++ src/JWTService.php
@@ -4,13 +4,15 @@ namespace Consilience\LaravelJWT;
 
 use Validator;
 use Firebase\JWT\JWT;
-use Firebase\JWT\BeforeValidException;
 
 class JWTService
 {
     public $key;
     public $algo;
     public $payload;
+    public $jwt;
+
+    // TODO: Bind this as a singleton to the service container.
 
     public function __construct()
     {
@@ -20,8 +22,25 @@ class JWTService
         JWT::$leeway = config('jwt.leeway', 0);
     }
 
-    public function parse(string $jwt)
+    public function getToken()
     {
+        return $this->jwt;
+    }
+
+    public function createToken($payload)
+    {
+        $jwt = JWT::encode($payload, $this->key, $this->algo);
+        
+        $this->jwt = $jwt;
+        $this->payload = $payload;
+
+        return $jwt;
+    }
+
+    public function parseToken(string $jwt)
+    {
+        $this->jwt = $jwt;
+
         if ($this->algo === 'HS256' || $this->algo === 'HS384' || $this->algo === 'HS512') {
             $this->payload = JWT::decode($jwt, $this->key, [$this->algo]);
         } else {
@@ -37,9 +56,9 @@ class JWTService
         return $this->payload;
     }
 
-    public function validate(array $rules)
+    public function validateToken($jwt, array $rules)
     {
-        $validator = Validator::make((array)$this->payload, $rules);
+        $validator = Validator::make((array)$jwt, $rules);
 
         if ($validator->fails()) {
             return false;

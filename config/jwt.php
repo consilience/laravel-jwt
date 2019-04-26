7b204f6afa71acb115ad19501934fdbb6aedc5dc
Michael Stokoe <mstokoe0990@gmail.com>
2019-04-26 11:16:39 +0100

    Not sure

--- config/jwt.php
+++ config/jwt.php
@@ -3,11 +3,11 @@
 return [
     'secret' => env('JWT_SECRET'),
     'leeway' => env('JWT_LEEWAY_SECONDS', 60),
-    'algo' => env('JWT_ALGO', 'RS256'),
+    'algo' => env('JWT_ALGO', 'HS256'),
 
     'keys' => [
-        'public' => env('JWT_PUBLIC_KEY', 'certs/jwtRS256.key.pub'),
-        'private' => env('JWT_PRIVATE_KEY', 'certs/jwtRS256.key'),
+        'public' => env('JWT_PUBLIC_KEY'),
+        'private' => env('JWT_PRIVATE_KEY'),
         'passphrase' => env('JWT_PASSPHRASE'),
     ],
 ];
